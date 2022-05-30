66b80e2b3670ea35673d57c1b7da1d05ba95c213
Bogdan Nagirniak <bnagirniak@luxoft.com>
2022-05-30 11:53:56 +0300

    Removing node properties as not needed

--- extern/hdusd/addon/properties/__init__.py
+++ extern/hdusd/addon/properties/__init__.py
@@ -22,7 +22,7 @@ class HdUSDProperties(bpy.types.PropertyGroup):
         del cls.bl_type.hdusd
 
 
-from . import node
+#from . import node
 
 register, unregister = bpy.utils.register_classes_factory((
     #node.NodeProperties,

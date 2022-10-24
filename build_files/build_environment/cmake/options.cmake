1c2fd54e6f2ff8ff5de95cb5faf9088f1e39d8a3
Bogdan Nagirniak <bnagirniak@luxoft.com>
2022-10-24 12:22:45 +0300

    BLEN-244: Improving HdRPR USDHydra addon. Fixed USD build for Blender

--- build_files/build_environment/cmake/options.cmake
+++ build_files/build_environment/cmake/options.cmake
@@ -3,7 +3,7 @@
 if(WIN32)
   option(ENABLE_MINGW64 "Enable building of ffmpeg/iconv/libsndfile/fftw3 by installing mingw64" ON)
 endif()
-option(WITH_BOOST_PYTHON "Enable building of boost with python support" OFF)
+option(WITH_BOOST_PYTHON "Enable building of boost with python support" ON)
 cmake_host_system_information(RESULT NUM_CORES QUERY NUMBER_OF_LOGICAL_CORES)
 set(MAKE_THREADS ${NUM_CORES} CACHE STRING "Number of threads to run make with")
 

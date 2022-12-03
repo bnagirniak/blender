105f0bb5fed60228511fd624fed8625296a826c3
Bogdan Nagirniak <bnagirniak@luxoft.com>
2022-12-03 14:29:34 +0200

    BLEN-279: Create initial BlenderSceneDelegate. Initial code

---
+++ extern/usdhydra/sceneDelegate/blenderSceneDelegate.cpp
@@ -0,0 +1,116 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright 2011-2022 Blender Foundation */
+
+#include <iostream>
+#include "blenderSceneDelegate.h"
+
+namespace usdhydra {
+
+BlenderSceneDelegate::BlenderSceneDelegate(HdRenderIndex* parentIndex, SdfPath const& delegateID, BL::Depsgraph &b_depsgraph)
+  : HdSceneDelegate(parentIndex, delegateID)
+  , _depsgraph(b_depsgraph)
+  , _isPopulated(false)
+{
+}
+
+void BlenderSceneDelegate::Sync(HdSyncRequestVector* request)
+{
+  std::cout << "Sync " << _isPopulated << "\n";
+
+  if (_isPopulated) {
+    return;
+  }
+
+  auto &instances = _depsgraph.object_instances;
+  for (auto& inst : instances) {
+    if (inst.is_instance()) {
+      continue;
+    }
+    
+    BL::Object obj = inst.object();
+    SdfPath objId = GetDelegateID().AppendElementString(obj.name_full());
+    
+    if (obj.type() == BL::Object::type_MESH) {
+      GetRenderIndex().InsertRprim(HdPrimTypeTokens->mesh, this, objId);
+      continue;
+    }
+    if (obj.type() == BL::Object::type_LIGHT) {
+      GetRenderIndex().InsertSprim(HdPrimTypeTokens->light, this, objId);
+      continue;
+    }
+    if (obj.type() == BL::Object::type_CAMERA) {
+      GetRenderIndex().InsertSprim(HdPrimTypeTokens->camera, this, objId);
+      continue;
+    }
+
+  }
+  _isPopulated = true;
+}
+
+HdMeshTopology BlenderSceneDelegate::GetMeshTopology(SdfPath const& id)
+{
+  std::cout << "GetMeshTopology: " << id.GetAsString() << "\n";
+
+  HdMeshTopology topology;
+  return topology;
+}
+
+VtValue BlenderSceneDelegate::Get(SdfPath const& id, TfToken const& key)
+{
+  std::cout << "Get: " << id.GetAsString() << " [" << key.GetString() << "]\n";
+
+  // tasks
+  _ValueCache *vcache = TfMapLookupPtr(_valueCacheMap, id);
+  VtValue ret;
+  if (vcache && TfMapLookup(*vcache, key, &ret)) {
+      return ret;
+  }
+
+  //// prims
+  //if (key == HdTokens->points) {
+  //    if(_meshes.find(id) != _meshes.end()) {
+  //        return VtValue(_meshes[id].points);
+  //    }
+  //} else if (key == HdTokens->displayColor) {
+  //    if(_meshes.find(id) != _meshes.end()) {
+  //        return VtValue(_meshes[id].color);
+  //    }
+  //} else if (key == HdTokens->displayOpacity) {
+  //    if(_meshes.find(id) != _meshes.end()) {
+  //        return VtValue(_meshes[id].opacity);
+  //    }
+  //} else if (key == HdInstancerTokens->scale) {
+  //    if (_instancers.find(id) != _instancers.end()) {
+  //        return VtValue(_instancers[id].scale);
+  //    }
+  //} else if (key == HdInstancerTokens->rotate) {
+  //    if (_instancers.find(id) != _instancers.end()) {
+  //        return VtValue(_instancers[id].rotate);
+  //    }
+  //} else if (key == HdInstancerTokens->translate) {
+  //    if (_instancers.find(id) != _instancers.end()) {
+  //        return VtValue(_instancers[id].translate);
+  //    }
+  //}
+  return VtValue();
+}
+
+GfMatrix4d BlenderSceneDelegate::GetTransform(SdfPath const& id)
+{
+  std::cout << "GetTransform: " << id.GetAsString() << "\n";
+  return GfMatrix4d(1);
+}
+
+VtValue BlenderSceneDelegate::GetCameraParamValue(SdfPath const& id, TfToken const& key)
+{
+  std::cout << "GetCameraParamValue: " << id.GetAsString() << " [" << key.GetString() << "]\n";
+  return VtValue();
+}
+
+VtValue BlenderSceneDelegate::GetLightParamValue(SdfPath const& id, TfToken const& key)
+{
+  std::cout << "GetLightParamValue: " << id.GetAsString() << " [" << key.GetString() << "]\n";
+  return VtValue();
+}
+
+} // namespace usdhydra
